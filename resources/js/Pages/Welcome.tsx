3e8e2e4bf50750200a71b032f3bf6f861b8fce32
alonfl <alon@example.com>
2026-01-22 17:32:59 +0200

    Initial commit: Laravel 11 + React + Inertia + Tailwind

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <div className="flex h-screen items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">
+                Welcome to your new Laravel + React + Tailwind app!
+            </h1>
+        </div>
+    );
+}
